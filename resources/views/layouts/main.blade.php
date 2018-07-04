8331bdf2a9d0fb1878185c3e154b7ac3ff155400
samhuhk@gmail.com <HLSAMHU@hkhs.com>
2018-07-04 12:31:40 +0800

    ITS

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -133,6 +133,26 @@
             }
         }
 
+        #top {
+          display: none;
+          position: fixed;
+          bottom: 20px;
+          right: 30px;
+          z-index: 99;
+          font-size: 18px;
+          border: none;
+          outline: none;
+          background-color: red;
+          color: white;
+          cursor: pointer;
+          padding: 15px;
+          border-radius: 4px;
+        }
+
+        #top:hover {
+          background-color: #555;
+        }
+
     </style>
 </head>
 <body>
@@ -165,6 +185,9 @@
             </form>
         </div>
     @endif
+
+    <button onclick="topFunction()" id="top" title="Go to top">置頂</button>
+
     @include('layouts.script')
     <script src="https://cdn.rawgit.com/vast-engineering/jquery-popup-overlay/1.7.13/jquery.popupoverlay.js"></script>
     <script>
@@ -297,6 +320,24 @@
                 },
             });
         });
+    </script>
+    <script>
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {scrollFunction()};
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                document.getElementById("top").style.display = "block";
+            } else {
+                document.getElementById("top").style.display = "none";
+            }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
     </script>
 </div>
 </body>
