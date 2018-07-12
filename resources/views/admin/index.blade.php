35ef88124dc57479e21e59d76bf7ef66564859a0
samhuhk@gmail.com <HLSAMHU@hkhs.com>
2018-07-12 14:33:40 +0800

    updated

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -97,6 +97,7 @@
                             <li class="active"><a href="#revenue-chart" data-toggle="tab">Area</a></li>
                             <li><a href="#sales-chart" data-toggle="tab">Donut</a></li>
                             <li class="pull-left header"><i class="fa fa-inbox"></i> Sales</li>
+                            <li><a href="#sales-bar" data-toggle="tab">Bar</a></li>
                         </ul>
                         <div class="tab-content no-padding">
                             <!-- Morris chart - Sales -->
@@ -233,11 +234,34 @@
             hideHover: 'auto'
         });
 
+        var bar = new Morris.Bar({
+            element   : 'sales-bar',
+            resize    : true,
+            data      : [
+                { y: '2011 Q1', item1: 2666, item2: 2666 },
+                { y: '2011 Q2', item1: 2778, item2: 2294 },
+                { y: '2011 Q3', item1: 4912, item2: 1969 },
+                { y: '2011 Q4', item1: 3767, item2: 3597 },
+                { y: '2012 Q1', item1: 6810, item2: 1914 },
+                { y: '2012 Q2', item1: 5670, item2: 4293 },
+                { y: '2012 Q3', item1: 4820, item2: 3795 },
+                { y: '2012 Q4', item1: 15073, item2: 5967 },
+                { y: '2013 Q1', item1: 10687, item2: 4460 },
+                { y: '2013 Q2', item1: 8432, item2: 5713 }
+            ],
+            xkey      : 'y',
+            ykeys     : ['item1', 'item2'],
+            labels    : ['Item 1', 'Item 2'],
+            lineColors: ['#a0d0e0', '#3c8dbc'],
+            hideHover : 'auto'
+        });
+
         // Fix for charts under tabs
         $('.box ul.nav a').on('shown.bs.tab', function () {
             area.redraw();
             donut.redraw();
             line.redraw();
+            bar.redraw();
         });
     </script>
 @stop
